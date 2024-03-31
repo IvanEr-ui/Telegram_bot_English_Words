0052ae286b5e3cd02f52442c76191e612ca5e4a4
Ivan <79088192+IvanEr-ui@users.noreply.github.com>
2024-03-31 15:09:57 +0300

    Update Database.py

--- Database.py
+++ Database.py
@@ -37,14 +37,14 @@ class Database:
             
             query = (f"INSERT INTO users (id, topic_id, questions_number, correct_answers_number) "
                      f"VALUES (%(user_id)s, %(topic_id)s, %(questions_number)s, %(correct_answers_number)s)")
-             print(query)
+            print(query)
             data = {
                 "user_id": user_id,
                 "topic_id": topic_id,
                 "questions_number": 5,
                 "correct_answers_number": 5,
             }
-             print(data)
+            print(data)
             self.cursor.execute(query, data)
             self.connection.commit()
         except (Exception, Error) as error:
